0b22f91e0d7ef852487a8e6bb11a0a3b0235d396
Andreas Peters <ap@aventer.biz>
2018-12-06 12:03:20 +0100

    export log

--- logging.go
+++ logging.go
@@ -4,19 +4,19 @@ import (
 	"log/syslog"
 
 	prefixed "git.aventer.biz/AVENTER/go-logrus-formatter"
-	log "github.com/sirupsen/logrus"
+	Log "github.com/sirupsen/logrus"
 	logrusSyslog "github.com/sirupsen/logrus/hooks/syslog"
 )
 
 // SetLogging sets the loglevel and text formating
-func SetLogging(level log.Level, enableSyslog bool, name string) {
-	log.SetFormatter(&prefixed.TextFormatter{
+func SetLogging(level Log.Level, enableSyslog bool, name string) {
+	Log.SetFormatter(&prefixed.TextFormatter{
 		ForceColors: true,
 	})
-	log.SetLevel(level)
+	Log.SetLevel(level)
 
 	if enableSyslog {
 		hook, _ := logrusSyslog.NewSyslogHook("", "", syslog.LOG_DEBUG, name)
-		log.AddHook(hook)
+		Log.AddHook(hook)
 	}
 }
