978eb2b9b72f8c07d5a8f9b190d1628f85457a86
Andreas Peters <ap@aventer.biz>
2022-08-25 00:19:22 +0200

    ADD: vault for env.

--- env.go
+++ env.go
@@ -1,12 +1,32 @@
 package util
 
-import "os"
+import (
+	"os"
+	"strings"
+	"time"
+
+	"github.com/AVENTER-UG/util/vault"
+)
 
 // Getenv with default value
 func Getenv(key, fallback string) string {
 	value := os.Getenv(key)
+
 	if len(value) == 0 {
 		return fallback
 	}
+
+	if strings.Contains(value, "vault://") {
+		VaultToken := os.Getenv("VAULT_TOKEN")
+		VaultURL := Getenv("VAULT_URL", "http://127.0.0.1:8200")
+		VaultTimeout, _ = time.ParseDuration(Getenv("VAULT_TIMEOUT", "10s"))
+
+		v := vault.New(VaultToken, VaultURL, VaultTimeout)
+		if config.VaultToken != "" {
+			v.Connect()
+			value = v.GetKey(value)
+		}
+	}
+
 	return value
 }
